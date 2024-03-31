cff4ad42641c510fb330cf55a6c7123cb7df9f0c
osipovpa <p82o@yandex.ru>
2024-03-31 16:47:52 +0300

    localization refactoring

--- app/extras/localizator.py
+++ app/extras/localizator.py
@@ -1,38 +1,46 @@
-import os
 import csv
-from typing import Any
+from pathlib import Path
+from typing import Any, Iterable
+
+LOCALES_KEY = "locale"
 
 
 class Localizator:
-    def __init__(self, filepath) -> None:
+    def __init__(self, filepath: Path, default_lang: str = "ru") -> None:
         self.__filepath = filepath
-        if not os.path.exists(self.__filepath):
+        self.__default_lang = default_lang.lower()
+        if not self.__filepath.exists():
             raise Exception("Locales file not found!")
-        self.__langs: dict = self.__get_lags_keys()
-        with open(self.__filepath, encoding="utf-8") as csvfile:
-            langs_reader = csv.reader(csvfile, delimiter=",", quotechar='"')
-            locales = list(langs_reader)
-            lang_keys = [x.lower() for x in locales[0]]
-            lang_keys.pop(0)
-            for lang_key in lang_keys:
-                for locale in locales:
-                    self.__langs[lang_key][locale[0]] = locale[
-                        lang_keys.index(lang_key) + 1
-                    ]
+        self.reload_locales()
 
-    def __get_lags_keys(self) -> dict:
-        with open(self.__filepath, encoding="utf-8") as csvfile:
-            langs_reader = csv.reader(csvfile, delimiter=",", quotechar='"')
-            locales = list(langs_reader)
-            locales[0].pop(0)
-            langs = {}
-            for lang_key in locales[0]:
-                langs[lang_key.lower()] = {}
-            return langs
+    def reload_locales(self) -> None:
+        self.__texts, self.__locales = self.__read_locales()
 
-    def get_text(self, key: str, *params: Any) -> str:
-        lang = "ru"
-        if key in self.__langs[lang]:
-            return str(self.__langs[lang][key]).format(*params)
-        else:
+    def available_locales(self) -> tuple[str, ...]:
+        return tuple(self.__locales.keys())
+
+    def available_keys(self) -> tuple[str, ...]:
+        return tuple(self.__texts.keys())
+
+    def get_text(self, key: str, *args: Any, lang: str | None = None) -> str:
+        _lang = lang.lower() if lang else self.__default_lang
+        if (
+            (locale_id := self.__locales.get(_lang)) is None
+            or (text := self.__texts.get(key)) is None
+        ):
             return key
+        return text[locale_id].format(*args) if args else text[locale_id]
+
+    def __read_locales(self) -> tuple[dict[str, tuple[str, ...]], dict[str, int]]:
+        __texts: dict[str, tuple[str, ...]] = {}
+        with self.__filepath.open(encoding="utf-8") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=",", quotechar='"')
+            for row in csv_reader:
+                key, *texts = row
+                __texts[key] = tuple(texts)
+        if LOCALES_KEY not in __texts:
+            raise Exception("Locales names not found.")
+        return __texts, self.__available_locales(__texts[LOCALES_KEY])
+
+    def __available_locales(self, locales: Iterable[str]) -> dict[str, int]:
+        return {locale.lower(): i for i, locale in enumerate(locales)}
